92203ae7ac99da2d10e6782aa24009756405655f
pbzweihander <pbzweihander@gmail.com>
2019-03-06 17:56:00 +0900

    brand new flip_vertically with slice split_at_mut and swap_with_slice

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(test)]
+
 use {
     png::{Encoder, HasParameters},
     std::io::Write,
@@ -40,7 +42,8 @@ impl Image {
         }
     }
 
-    fn coord(&self, x: u32, y: u32) -> usize {
+    #[inline]
+    const fn coord(&self, x: u32, y: u32) -> usize {
         (self.width * y + x) as usize
     }
 
@@ -54,7 +57,8 @@ impl Image {
         self
     }
 
-    pub fn flip_vertically(&mut self) -> &mut Self {
+    #[cfg(test)]
+    fn flip_vertically_orig(&mut self) -> &mut Self {
         let half = self.height >> 1;
 
         for y in 0..half {
@@ -69,11 +73,28 @@ impl Image {
         self
     }
 
+    pub fn flip_vertically(&mut self) -> &mut Self {
+        let half = self.height >> 1;
+
+        for y in 0..half {
+            let top_left = self.coord(0, y);
+            let bottom_right = self.coord(self.width, self.height - y - 1);
+
+            let s = &mut self.data[top_left..bottom_right];
+            let (top_line, s) = s.split_at_mut(self.width as usize);
+            let (_, bottom_line) = s.split_at_mut(s.len() - self.width as usize);
+
+            top_line.swap_with_slice(bottom_line);
+        }
+
+        self
+    }
+
     pub fn flip_horizontally(&mut self) -> &mut Self {
         let half = self.width >> 1;
 
-        for x in 0..half {
-            for y in 0..self.height {
+        for y in 0..self.height {
+            for x in 0..half {
                 let left_line = self.coord(x, y);
                 let right_line = self.coord(self.width - x - 1, y);
 
@@ -97,5 +118,66 @@ impl Image {
             .collect();
         writer.write_image_data(&data)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+
+    use {super::*, test::Bencher};
+
+    fn make_image() -> Image {
+        let mut image = Image::new(255, 255);
+
+        for y in 0..255 {
+            for x in 0..255 {
+                image.set(
+                    x,
+                    y,
+                    Color::from_rgba((x) as u8, (255 - x) as u8, (y) as u8, (255 - y) as u8),
+                );
+            }
+        }
+
+        image
+    }
+
+    #[test]
+    fn test_flip_vertically() {
+        let mut image = make_image();
+        let mut image1 = image.clone();
+        let mut data: Vec<Vec<_>> = image.data.clone().chunks(255).map(|s| s.to_vec()).collect();
+
+        image.flip_vertically();
+        image1.flip_vertically_orig();
+        data.reverse();
+
+        assert_eq!(
+            image.data,
+            data.clone()
+                .into_iter()
+                .flat_map(IntoIterator::into_iter)
+                .collect::<Vec<_>>(),
+        );
+        assert_eq!(image.data, image1.data);
+    }
+
+    #[bench]
+    fn bench_flip_vertically(b: &mut Bencher) {
+        let mut image = make_image();
+
+        b.iter(|| {
+            image.flip_vertically();
+        });
+    }
+
+    #[bench]
+    fn bench_flip_vertically_orig(b: &mut Bencher) {
+        let mut image = make_image();
+
+        b.iter(|| {
+            image.flip_vertically_orig();
+        });
     }
 }
