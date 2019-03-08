2c3cbc904125340e2bba5de8f8fa44d4246be256
pbzweihander <pbzweihander@gmail.com>
2019-03-08 17:09:27 +0900

    wireframe rendering

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![feature(test)]
 
 use {
-    std::{env::args, fs::File, path::Path},
+    obj::*,
+    std::{env::args, fs::File, io::BufReader},
     tinyrenderer::{Color, Error, Image},
 };
 
-const RED: Color = Color::from_rgba(255, 0, 0, 255);
+const BLACK: Color = Color::from_rgba(0, 0, 0, 255);
 const WHITE: Color = Color::from_rgba(255, 255, 255, 255);
 
 fn diff(a: u32, b: u32) -> u32 {
@@ -57,15 +58,36 @@ fn line(x0: u32, y0: u32, x1: u32, y1: u32, image: &mut Image, color: Color) {
 fn main() -> Result<(), Error> {
     let mut args = args();
     args.next();
-    let filename = args.next().unwrap_or_else(|| "output.png".to_string());
-    let filepath = Path::new(&filename);
-    let file = File::create(filepath)?;
 
-    let mut image = Image::new(100, 100);
-    line(13, 20, 80, 40, &mut image, WHITE);
-    line(20, 13, 40, 80, &mut image, RED);
-    line(80, 40, 13, 20, &mut image, RED);
-    image.flip_vertically().write_png(file)?;
+    let obj_file_name = args
+        .next()
+        .unwrap_or_else(|| "obj/african_head.obj".to_string());
+    let obj_file = File::open(&obj_file_name)?;
+    let obj_file_buf_reader = BufReader::new(obj_file);
+    let model: Obj = load_obj(obj_file_buf_reader)?;
+
+    let out_file_name = args.next().unwrap_or_else(|| "output.png".to_string());
+    let out_file = File::create(&out_file_name)?;
+
+    let mut image = Image::with_background(800, 800, BLACK);
+
+    for face in model.indices.chunks_exact(3) {
+        for &(i, j) in &[(0, 1), (1, 2), (2, 0)] {
+            let [x0, y0, _] = model.vertices[face[i] as usize].position;
+            let [x1, y1, _] = model.vertices[face[j] as usize].position;
+
+            let (x0, x1, y0, y1) = (
+                ((x0 + 1f32) * image.width as f32 / 2f32) as u32,
+                ((x1 + 1f32) * image.width as f32 / 2f32) as u32,
+                ((y0 + 1f32) * image.height as f32 / 2f32) as u32,
+                ((y1 + 1f32) * image.height as f32 / 2f32) as u32,
+            );
+
+            line(x0, y0, x1, y1, &mut image, WHITE);
+        }
+    }
+
+    image.flip_vertically().write_png(out_file)?;
 
     Ok(())
 }
@@ -75,9 +97,12 @@ mod tests {
     extern crate test;
 
     use {
-        super::{line, Image, RED, WHITE},
+        super::{line, Color, Image},
         test::Bencher,
     };
+
+    const RED: Color = Color::from_rgba(255, 0, 0, 255);
+    const WHITE: Color = Color::from_rgba(255, 255, 255, 255);
 
     #[bench]
     fn bench_line(b: &mut Bencher) {
